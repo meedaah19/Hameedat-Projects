a77f84e1dea3fc6d32b3244f1c2a9e8a3d3a2e58
Hameedat Oyewopo Timileyin <161971114+meedaah19@users.noreply.github.com>
2024-05-19 12:40:37 +0100

    Add files via upload

---
+++ Dicee Challenge/index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource = "images/dice" + randomNumber1 + '.png';
+
+var image = document.querySelectorAll("img")[0];
+
+image.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource = "images/dice" + randomNumber2 + '.png';
+
+var image = document.querySelectorAll("img")[1];
+
+image.setAttribute("src", randomImageSource);
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = ("🚩 Play 1 Wins!");
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = ("🚩 Play 2 Wins!");
+}
+else {
+    document.querySelector("h1").innerHTML = ("Draw!");
+}
